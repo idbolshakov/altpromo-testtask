d1d47cc51ba03394b5127032c1567854a8d48c3f
Ivan Bolshakov <idbolshakov@gmail.com>
2016-10-10 02:45:13 +0500

    Added configuration page for module

--- altpromotest.php
+++ altpromotest.php
@@ -44,6 +44,7 @@ class AltpromoTest extends Module {
     /**
      * install
      *
+     * Метод отвечает за установку модуля
      */
     public function install() {
 
@@ -67,6 +68,7 @@ class AltpromoTest extends Module {
     /**
      * uninstall
      *
+     * метод отвечает за удаление модуля
      */
     public function uninstall() {
 
@@ -91,8 +93,8 @@ class AltpromoTest extends Module {
         $this->context->smarty->assign(
 
             array(
-                'altpromotest_low'  =>  '0',
-                'altpromotest_high' => '100'
+                'altpromotest_low'  => Configuration::get('altpromotest_low'),
+                'altpromotest_high' => Configuration::get('altpromotest_high')
             )
         );
 
@@ -121,6 +123,130 @@ class AltpromoTest extends Module {
             ->context
             ->controller
             ->addCSS($this->_path.'css/altpromotest.css', 'all');
+    }
+
+    /**
+     * getContent
+     *
+     * вызывается, когда страница 
+     * настройки модуля загружена из бэк-офиса
+     */
+    public function getContent() {
+
+        $output = null;
+
+        if (Tools::isSubmit('submit'.$this->name)) {
+
+            // low value
+            $low_value = strval(Tools::getValue('altpromotest_low'));
+
+            if (!$low_value
+                || empty($low_value)
+                || !Validate::isGenericName($low_value)
+                || !is_numeric($low_value)) {
+
+                $output .= $this->displayError(
+                    $this->l('Неверный формат значения "от"')
+                );
+
+            } else {
+
+                Configuration::updateValue('altpromotest_low', $low_value);
+
+                $output .= $this->displayConfirmation(
+                    $this->l('Значение "от" успешно установлено')
+                );
+            }
+
+            // high value
+            $high_value = strval(Tools::getValue('altpromotest_high'));
+
+            if (!$high_value
+                || empty($high_value)
+                || !Validate::isGenericName($high_value)
+                || !is_numeric($high_value)) {
+
+                $output .= $this->displayError(
+                    $this->l('Неверный формат значения "до"')
+                );
+
+            } else {
+
+                Configuration::updateValue('altpromotest_high', $high_value);
+
+                $output .= $this->displayConfirmation(
+                    $this->l('Значение "до" успешно установлено')
+                );
+            }
+        }
+        
+        return $output.$this->displayForm();
+    }
+
+    /**
+     * displayForm
+     *
+     * показываем форму конфигурации модуля
+     * в бэк-офисе
+     */
+    public function displayForm() {
+
+        $default_lang = (int) Configuration::get('PS_LANG_DEFAULT');
+
+        $fields_form[0]['form'] = array(
+
+            'legend' => array(
+
+                'title' => $this->l('Настройки модуля'),
+            ),
+
+            'input' => array(
+
+                array(
+
+                    'type'     => 'text',
+                    'label'    => $this->l('от'),
+                    'name'     => 'altpromotest_low',
+                    'size'     => 10,
+                    'required' => true
+                ),
+
+                array(
+
+                    'type'     => 'text',
+                    'label'    => $this->l('до'),
+                    'name'     => 'altpromotest_high',
+                    'size'     => 10,
+                    'required' => true
+                )
+            ),
+
+            'submit' => array(
+
+                'title' => $this->l('Применить'),
+                'class' => 'btn btn-default pull-right'
+            )
+        );
+
+        $helper = new HelperForm();
+
+        $helper->module          = $this;
+        $helper->name_controller = $this->name;
+        $helper->token           = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex    = 
+            AdminCOntroller::$currentIndex.'&configure='.$this->name;
+
+        $helper->submit_action   = 'submit'.$this->name;
+
+
+        $helper->fields_value['altpromotest_low'] 
+            = Configuration::get('altpromotest_low');
+
+        $helper->fields_value['altpromotest_high'] 
+            = Configuration::get('altpromotest_high');
+
+
+        return $helper->generateForm($fields_form);
     }
 }
 ?>
